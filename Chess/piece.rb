8b895d7ef906167cd6158680b68a582484e326bb
Mike Criollo <mkcriollo@gmail.com>
2020-11-17 17:38:29 -0500

    Make moves for Piece class

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,5 +1,22 @@
+require_relative "board.rb"
+
 class Piece
-    def initializ
-        
+    def initialize
+        @board = Board.new()
     end
+
+    def moves
+        valid_moves = []
+
+        # board1 = Board.new
+
+        @board.rows.each_with_index do |row, idx1|
+            row.each_with_index do |ele, idx2|
+                valid_moves << [idx1,idx2] if @board[[idx1,idx2]].class == NullPiece 
+            end
+        end
+        valid_moves
+    end
+
+
 end
